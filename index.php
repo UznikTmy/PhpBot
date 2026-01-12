b85856af3e65ded755bb68468d956ecc17c2b8f4
UznikTmy <lm8897715@gmail.com>
2026-01-12 05:31:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
 echo('Hello, world!');
-
- 
-$chat_name = "@topicdaily";
-$token = "8127692420:AAE9KspSYSY8WnJ5FBEMTY5hJcW14pku6d4";
-$message = "Здравствуйте.\nЭто тестовое сообщение, отправленное ботом с помощью PHP-скрипта.\n\nВсем хорошего дня.";
- 
-$text = urlencode($message);
-$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_name}&text={$text}";
- 
-$ch = curl_init();
-$optArray = array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true
-);
-curl_setopt_array($ch, $optArray);
-$result = curl_exec($ch);
-curl_close($ch);
-
 ?>
